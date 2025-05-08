b84f3ea066cb7069ea528cda5d4910d5656e3d93
Adarsh-2003 <adarshgupta.contact@gmail.com>
2025-05-08 22:28:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <GitHubCalendar username="adarsh-2003" />
+    </div>
+  );
+}
+
+export default App;
